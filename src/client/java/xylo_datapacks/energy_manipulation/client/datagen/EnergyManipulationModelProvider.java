e3363db44b0ca7cf6f80da23c228b617b898ec57
XyloDatapacks <xylo.datapacks@gmail.com>
2026-05-11 20:55:53 +0200

    updated datagen to scale up buttons that use glyph like textures

--- src/client/java/xylo_datapacks/energy_manipulation/client/datagen/EnergyManipulationModelProvider.java
+++ src/client/java/xylo_datapacks/energy_manipulation/client/datagen/EnergyManipulationModelProvider.java
@@ -8,14 +8,17 @@ import net.minecraft.client.data.models.model.ItemModelUtils;
 import net.minecraft.client.data.models.model.ModelTemplates;
 import net.minecraft.client.data.models.model.TextureMapping;
 import net.minecraft.client.data.models.model.TextureSlot;
+import net.minecraft.client.renderer.item.ClientItem;
 import net.minecraft.client.renderer.item.ItemModel;
 import net.minecraft.client.renderer.item.SelectItemModel;
 import net.minecraft.client.renderer.item.properties.select.CustomModelDataProperty;
 import net.minecraft.client.resources.model.sprite.Material;
 import net.minecraft.resources.Identifier;
 import net.minecraft.world.item.Item;
+import org.joml.Vector3f;
 import org.jspecify.annotations.NonNull;
 import xylo_datapacks.energy_manipulation.EnergyManipulation;
+import xylo_datapacks.energy_manipulation.client.utils.EnergyManipulationModelTemplate;
 import xylo_datapacks.energy_manipulation.glyph.GlyphsRegistry;
 import xylo_datapacks.energy_manipulation.glyph.specialized.operation.OperatorGlyphInterface;
 import xylo_datapacks.energy_manipulation.glyph.value_type.GlyphValueType;
@@ -24,7 +27,6 @@ import xylo_datapacks.energy_manipulation.spell_editor.SpellEditorButtonsRegistr
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 public class EnergyManipulationModelProvider extends FabricModelProvider {
     
@@ -46,44 +48,56 @@ public class EnergyManipulationModelProvider extends FabricModelProvider {
 
     public void generateDynamicGuiItemModels(ItemModelGenerators generator, Item baseItem) {
         List<String> allButtons = new ArrayList<>();
+        List<String> allGlyphs = new ArrayList<>();
         
         // Static buttons
         allButtons.addAll(SpellEditorButtonsRegistry.SPELL_EDITOR_BUTTON.keySet().stream().map(Identifier::getPath).toList());
+        
+        
         // Glyphs
-        allButtons.addAll(GlyphsRegistry.GLYPH.keySet().stream().map(Identifier::getPath).toList());
+        allGlyphs.addAll(GlyphsRegistry.GLYPH.keySet().stream().map(Identifier::getPath).toList());
         // Value types
-        allButtons.addAll(GlyphsRegistry.VALUE_TYPE.keySet().stream().map(Identifier::getPath).toList());
+        allGlyphs.addAll(GlyphsRegistry.VALUE_TYPE.keySet().stream().map(Identifier::getPath).toList());
         // Type specific raw value glyphs
         String rawValueGlyphKey = GlyphsRegistry.GLYPH.getKey(GlyphsRegistry.RAW_VALUE_GLYPH).getPath();
-        allButtons.addAll(GlyphsRegistry.VALUE_TYPE.stream()
+        allGlyphs.addAll(GlyphsRegistry.VALUE_TYPE.stream()
                 .filter(GlyphValueType::hasValueSelector)
                 .map(valueType -> rawValueGlyphKey + "_" + GlyphsRegistry.VALUE_TYPE.getKey(valueType).getPath())
                 .toList());
         // Operator glyphs pin separator
-        allButtons.addAll(GlyphsRegistry.GLYPH.stream()
+        allGlyphs.addAll(GlyphsRegistry.GLYPH.stream()
                 .filter(OperatorGlyphInterface.class::isInstance)
                 .map(valueType -> GlyphsRegistry.GLYPH.getKey(valueType).getPath() + "_separator")
                 .toList());
+        // Glyph buttons (treating them as glyphs since they need to be scaled too)
+        allGlyphs.addAll(SpellEditorButtonsRegistry.SPELL_EDITOR_GLYPH_BUTTON.keySet().stream().map(Identifier::getPath).toList());
+
         
-        System.out.println("Generating models for buttons: " + allButtons);
-
-        // Generate individual model for each button.
+        // generate switch cases and their models
         List<SelectItemModel.SwitchCase<String>> switchCases = new ArrayList<>();
-        for (String buttonName : allButtons) {
-            Identifier modelIdentifier = Identifier.fromNamespaceAndPath(EnergyManipulation.MOD_ID, "item/gui/button/" + buttonName);
-
-            ModelTemplates.FLAT_ITEM.create(modelIdentifier, TextureMapping.singleSlot(TextureSlot.LAYER0, new Material(modelIdentifier)), generator.modelOutput);
-            // ((EnergyManipulationModelTemplate) ModelTemplates.FLAT_ITEM).energy_manipulation$createWithGuiScale(modelIdentifier, TextureMapping.singleSlot(TextureSlot.LAYER0, new Material(modelIdentifier)), generator.modelOutput, new Vector3f(1.f, 1.f, 1.f));
-
-            ItemModel.Unbaked caseModel = ItemModelUtils.plainModel(modelIdentifier);
-            switchCases.add(ItemModelUtils.when(buttonName, caseModel));
-        }
+        createAndAddModelInstanceCases(generator, switchCases, allButtons, new Vector3f(1.f, 1.f, 1.f));
+        createAndAddModelInstanceCases(generator, switchCases, allGlyphs, new Vector3f(2.f, 2.f, 1.f));
 
         // Add fallback model for unknown buttons.
         ItemModel.Unbaked fallbackModel = ItemModelUtils.plainModel(Identifier.withDefaultNamespace("item/barrier"));
 
         // Create the main model for GuiButtonItem. It checks for custom data property 0.
-        generator.itemModelOutput.accept(baseItem, ItemModelUtils.select(new CustomModelDataProperty(0), fallbackModel, switchCases));
+        generator.itemModelOutput.accept(baseItem, ItemModelUtils.select(new CustomModelDataProperty(0), fallbackModel, switchCases), new ClientItem.Properties(true, true, 1.F));
+    }
+    
+    private void createAndAddModelInstanceCases(ItemModelGenerators generator, List<SelectItemModel.SwitchCase<String>> destination, List<String> buttonNames, Vector3f scale) {
+        for (String buttonName : buttonNames) {
+            Identifier modelIdentifier = Identifier.fromNamespaceAndPath(EnergyManipulation.MOD_ID, "item/gui/button/" + buttonName);
+
+            if (scale.equals(1.f, 1.f, 1.f)) {
+                ModelTemplates.FLAT_ITEM.create(modelIdentifier, TextureMapping.singleSlot(TextureSlot.LAYER0, new Material(modelIdentifier)), generator.modelOutput);
+            } else {
+                ((EnergyManipulationModelTemplate) ModelTemplates.FLAT_ITEM).energy_manipulation$createWithGuiScale(modelIdentifier, TextureMapping.singleSlot(TextureSlot.LAYER0, new Material(modelIdentifier)), generator.modelOutput, scale);
+            }
+
+            ItemModel.Unbaked caseModel = ItemModelUtils.plainModel(modelIdentifier);
+            destination.add(ItemModelUtils.when(buttonName, caseModel));
+        }
     }
     
 }
--- src/main/java/xylo_datapacks/energy_manipulation/spell_editor/SpellEditorButtonsRegistry.java
+++ src/main/java/xylo_datapacks/energy_manipulation/spell_editor/SpellEditorButtonsRegistry.java
@@ -4,10 +4,8 @@ import net.fabricmc.fabric.api.event.registry.FabricRegistryBuilder;
 import net.fabricmc.fabric.api.event.registry.RegistryAttribute;
 import net.minecraft.core.Registry;
 import net.minecraft.core.component.DataComponents;
-import net.minecraft.network.chat.Component;
 import net.minecraft.resources.Identifier;
 import net.minecraft.resources.ResourceKey;
-import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.component.CustomModelData;
 import xylo_datapacks.energy_manipulation.EnergyManipulation;
@@ -24,18 +22,22 @@ public class SpellEditorButtonsRegistry {
     // Buttons registry
     public static final ResourceKey<Registry<Supplier<ItemStack>>> SPELL_EDITOR_BUTTON_REGISTRY_KEY = ResourceKey.createRegistryKey(Identifier.fromNamespaceAndPath(EnergyManipulation.MOD_ID, "spell_editor_button_registry"));
     public static final Registry<Supplier<ItemStack>> SPELL_EDITOR_BUTTON = FabricRegistryBuilder.create(SPELL_EDITOR_BUTTON_REGISTRY_KEY).attribute(RegistryAttribute.OPTIONAL).buildAndRegister();
+    // Glyph buttons registry (fix for data gen since I do not have good textures for the glyphs and I need to scale them up more)
+    public static final ResourceKey<Registry<Supplier<ItemStack>>> SPELL_EDITOR_GLYPH_BUTTON_REGISTRY_KEY = ResourceKey.createRegistryKey(Identifier.fromNamespaceAndPath(EnergyManipulation.MOD_ID, "spell_editor_glyph_button_registry"));
+    public static final Registry<Supplier<ItemStack>> SPELL_EDITOR_GLYPH_BUTTON = FabricRegistryBuilder.create(SPELL_EDITOR_GLYPH_BUTTON_REGISTRY_KEY).attribute(RegistryAttribute.OPTIONAL).buildAndRegister();
     
-    public static final Supplier<ItemStack> CONFIRM_BUTTON = registerGlyph("confirm_button", EnergyManipulationItems.GUI_BUTTON::getDefaultInstance);
-    public static final Supplier<ItemStack> CONFIRM_BUTTON_DISABLED = registerGlyph("confirm_button_disabled", EnergyManipulationItems.GUI_BUTTON::getDefaultInstance);
-    public static final Supplier<ItemStack> CANCEL_BUTTON = registerGlyph("cancel_button", EnergyManipulationItems.GUI_BUTTON::getDefaultInstance);
-    public static final Supplier<ItemStack> NEXT_PAGE_BUTTON = registerGlyph("next_page_button", EnergyManipulationItems.GUI_BUTTON::getDefaultInstance);
-    public static final Supplier<ItemStack> PREVIOUS_PAGE_BUTTON = registerGlyph("previous_page_button", EnergyManipulationItems.GUI_BUTTON::getDefaultInstance);
-    public static final Supplier<ItemStack> NEXT_PAGE_BUTTON_DISABLED = registerGlyph("next_page_button_disabled", EnergyManipulationItems.GUI_BUTTON::getDefaultInstance);
-    public static final Supplier<ItemStack> PREVIOUS_PAGE_BUTTON_DISABLED = registerGlyph("previous_page_button_disabled", EnergyManipulationItems.GUI_BUTTON::getDefaultInstance);
-    public static final Supplier<ItemStack> INSERT_OR_REMOVE_ELEMENT_BUTTON = registerGlyph("insert_or_remove_element_button", EnergyManipulationItems.GUI_BUTTON::getDefaultInstance);
-    public static final Supplier<ItemStack> ADD_ELEMENT_BUTTON = registerGlyph("add_element_button", EnergyManipulationItems.GUI_BUTTON::getDefaultInstance);
-    public static final Supplier<ItemStack> ARRAY_START_BUTTON = registerGlyph("array_start_button", EnergyManipulationItems.GUI_BUTTON::getDefaultInstance);
-    public static final Supplier<ItemStack> EMPTY_PIN_BUTTON = registerGlyph("empty_pin_button", EnergyManipulationItems.GUI_BUTTON::getDefaultInstance);
+    public static final Supplier<ItemStack> CONFIRM_BUTTON = registerButton("confirm_button", EnergyManipulationItems.GUI_BUTTON::getDefaultInstance);
+    public static final Supplier<ItemStack> CONFIRM_BUTTON_DISABLED = registerButton("confirm_button_disabled", EnergyManipulationItems.GUI_BUTTON::getDefaultInstance);
+    public static final Supplier<ItemStack> CANCEL_BUTTON = registerButton("cancel_button", EnergyManipulationItems.GUI_BUTTON::getDefaultInstance);
+    public static final Supplier<ItemStack> NEXT_PAGE_BUTTON = registerButton("next_page_button", EnergyManipulationItems.GUI_BUTTON::getDefaultInstance);
+    public static final Supplier<ItemStack> PREVIOUS_PAGE_BUTTON = registerButton("previous_page_button", EnergyManipulationItems.GUI_BUTTON::getDefaultInstance);
+    public static final Supplier<ItemStack> NEXT_PAGE_BUTTON_DISABLED = registerButton("next_page_button_disabled", EnergyManipulationItems.GUI_BUTTON::getDefaultInstance);
+    public static final Supplier<ItemStack> PREVIOUS_PAGE_BUTTON_DISABLED = registerButton("previous_page_button_disabled", EnergyManipulationItems.GUI_BUTTON::getDefaultInstance);
+    
+    public static final Supplier<ItemStack> INSERT_OR_REMOVE_ELEMENT_BUTTON = registerGlyphButton("insert_or_remove_element_button", EnergyManipulationItems.GUI_BUTTON::getDefaultInstance);
+    public static final Supplier<ItemStack> ADD_ELEMENT_BUTTON = registerGlyphButton("add_element_button", EnergyManipulationItems.GUI_BUTTON::getDefaultInstance);
+    public static final Supplier<ItemStack> ARRAY_START_BUTTON = registerGlyphButton("array_start_button", EnergyManipulationItems.GUI_BUTTON::getDefaultInstance);
+    public static final Supplier<ItemStack> EMPTY_PIN_BUTTON = registerGlyphButton("empty_pin_button", EnergyManipulationItems.GUI_BUTTON::getDefaultInstance);
     
     public static ItemStack getGlyphButtonStack(Glyph glyph, GlyphValueType outputValueType) {
         ItemStack glyphButtonStack = EnergyManipulationItems.GUI_BUTTON.getDefaultInstance();
@@ -59,7 +61,17 @@ public class SpellEditorButtonsRegistry {
         return operatorSeparatorButtonStack;
     }
 
-    public static Supplier<ItemStack> registerGlyph(String name, Supplier<ItemStack> factory) {
+    public static Supplier<ItemStack> registerGlyphButton(String name, Supplier<ItemStack> factory) {
+        Supplier<ItemStack> output = () -> {
+            ItemStack stack = factory.get();
+            setCustomModelData(stack, name);
+            return stack;
+        };
+        Registry.register(SPELL_EDITOR_GLYPH_BUTTON, Identifier.fromNamespaceAndPath(EnergyManipulation.MOD_ID, name), output);
+        return output;
+    }
+
+    public static Supplier<ItemStack> registerButton(String name, Supplier<ItemStack> factory) {
         Supplier<ItemStack> output = () -> {
             ItemStack stack = factory.get();
             setCustomModelData(stack, name);
