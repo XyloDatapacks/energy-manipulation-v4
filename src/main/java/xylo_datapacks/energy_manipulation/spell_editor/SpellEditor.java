8dc0a3d890746b08ce24c3ed60ad8c4b4aee942e
XyloDatapacks <xylo.datapacks@gmail.com>
2026-05-12 22:39:31 +0200

    spell editor now wraps current glyph instance inside a custom root glyph instance to allow for callbacks

--- src/main/java/xylo_datapacks/energy_manipulation/spell_editor/SpellEditor.java
+++ src/main/java/xylo_datapacks/energy_manipulation/spell_editor/SpellEditor.java
@@ -7,35 +7,97 @@ import xylo_datapacks.energy_manipulation.glyph.GlyphUtils;
 import xylo_datapacks.energy_manipulation.glyph.GlyphsRegistry;
 import xylo_datapacks.energy_manipulation.glyph.pin.InputPin;
 import xylo_datapacks.energy_manipulation.glyph.pin.InputPinDefinition;
+import xylo_datapacks.energy_manipulation.glyph.pin.InputPinMode;
 
 import java.util.Optional;
 import java.util.function.Consumer;
 
 public class SpellEditor {
+    static protected final SpellEditorRootGlyph ROOT_GLYPH = new SpellEditorRootGlyph();
+
+    protected final SpellEditorRootGlyphInstance rootGlyphInstance;
     protected GlyphInstance originalGlyphInstance;
     protected GlyphInstance currentGlyphInstance;
 
-    public SpellEditor() {
+    /*================================================================================================================*/
+    // RootGlyph
+    
+    protected static class SpellEditorRootGlyphInstance extends GlyphInstance {
+
+        public SpellEditorRootGlyphInstance(Glyph glyph) {
+            super(glyph);
+            this.onInstanceChangedCallback = () -> {};
+        }
+
+        Runnable onInstanceChangedCallback;
     }
+
+    protected static class SpellEditorRootGlyph extends Glyph {
+        static public String INPUT_PIN = "Input";
+        
+        public SpellEditorRootGlyph() {
+            super();
+
+            this.inputPinMode = InputPinMode.STANDARD;
+            this.RegisterPinDefinition(INPUT_PIN, glyph -> true);
+        }
+
+        @Override
+        protected GlyphInstance createInstance_internal() {
+            return new SpellEditorRootGlyphInstance(this);
+        }
+
+        @Override
+        public void initializePins(GlyphInstance glyphInstance) {
+            this.getInputPin(glyphInstance, INPUT_PIN).ifPresent(inputPin -> {
+                inputPin.valueType = glyphInstance.outputPin.valueType;
+            });
+        }
+
+        @Override
+        public void onDescendantGlyphStateChanged(GlyphInstance glyphInstance, GlyphInstance descendantInstance, int pinIndex) {
+            EnergyManipulation.LOGGER.warn("onDescendantGlyphStateChanged");
+            ((SpellEditorRootGlyphInstance) glyphInstance).onInstanceChangedCallback.run();
+        }
+    }
+
+    // ~RootGlyph
+    /*================================================================================================================*/
+
+    public SpellEditor() {
+        rootGlyphInstance = (SpellEditorRootGlyphInstance) ROOT_GLYPH.instantiate(GlyphsRegistry.EXECUTION_VALUE_TYPE);
+        rootGlyphInstance.onInstanceChangedCallback = this::onInstanceChanged;
+    }
+
+    /*================================================================================================================*/
+    // TrackedInstance
     
     public void reset() {
         this.currentGlyphInstance = null;
         this.originalGlyphInstance = null;
+        GlyphUtils.resetConnection(rootGlyphInstance, SpellEditorRootGlyph.INPUT_PIN);
     }
 
     public void initialize(GlyphInstance glyphInstance) {
         this.currentGlyphInstance = glyphInstance;
         this.originalGlyphInstance = GlyphUtils.copyGlyphInstance(glyphInstance);
+        GlyphUtils.connectGlyph(rootGlyphInstance, SpellEditorRootGlyph.INPUT_PIN, currentGlyphInstance);
+    }
+    
+    public void restoreGlyphInstance() {
+        currentGlyphInstance = GlyphUtils.copyGlyphInstance(originalGlyphInstance);
+        GlyphUtils.connectGlyph(rootGlyphInstance, SpellEditorRootGlyph.INPUT_PIN, currentGlyphInstance);
     }
 
     public GlyphInstance getCurrentGlyphInstance() {
         return currentGlyphInstance;
     }
-    
-    public void restoreGlyphInstance() {
-        currentGlyphInstance = GlyphUtils.copyGlyphInstance(originalGlyphInstance);;
-    }
 
+    public void onInstanceChanged() {}
+
+    // ~TrackedInstance
+    /*================================================================================================================*/
+    
     public String printCompatibleGlyphs(GlyphInstance glyphInstance, String pinName) {
         int pinIndex = glyphInstance.glyph.getInputPinIndex(pinName);
         return printCompatibleGlyphs(glyphInstance, pinIndex);
